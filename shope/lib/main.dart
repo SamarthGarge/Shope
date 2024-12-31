72b6c920b37facfa8a21b38ff04873be94f08299
Samarth Garge <gargesamarth@gmail.com>
2024-12-31 17:17:18 +0530

    init

---
+++ shope/lib/main.dart
@@ -0,0 +1,24 @@
+
+import 'package:shope/screens/splash_page.dart';
+import 'package:flutter/material.dart';
+
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shope',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        canvasColor: Colors.transparent,
+        primarySwatch: Colors.blue,
+        fontFamily: "Montserrat",
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
